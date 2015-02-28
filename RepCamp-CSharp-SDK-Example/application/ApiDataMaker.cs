8e790ad838b1f73e0001e201d1092aaf93ac7e13
Marc Mora <marc.mora.aparicio@gmail.com>
2015-02-28 01:06:50 +0100

    Revert "Get Customers added"
    This reverts commit 871244b031a9e7538a9a151d8cbf8e0d4646aa3c.

--- RepCamp-CSharp-SDK-Example/application/ApiDataMaker.cs
+++ RepCamp-CSharp-SDK-Example/application/ApiDataMaker.cs
@@ -13,18 +13,11 @@ namespace RepCampSDKExample.application
         {
             //API Data to be modified. Retrieve it from a *.properties file or just stick it in your code!
 
-            //String url = "http://api.repcamp.com";
-            //String username = "your@email.com";
-            //String password = "Sha1_encrypted_password";
-            //String secretkey = "your_secret_key";
-            //String apikey = "your_api_key";
-            //String apiversion = "v1";
-
             String url = "http://api.repcamp.com";
-            String username = "marc.mora@kriter.net";
-            String password = "7c222fb2927d828af22f592134e8932480637c0d";
-            String secretkey = "7677e9f1cdd28f672589ccb9740487be655ddfb254e73e2377f2f";
-            String apikey = "3ec02e37be25462caa22f98fc20d840b54e73e2377e8a";
+            String username = "your@email.com";
+            String password = "Sha1_encrypted_password";
+            String secretkey = "your_secret_key";
+            String apikey = "your_api_key";
             String apiversion = "v1";
 
             return new APIData(url, username,password,apikey,secretkey,apiversion);
